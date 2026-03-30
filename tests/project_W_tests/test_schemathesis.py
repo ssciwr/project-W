a9a8ca7a7d654c7c10f06aca297b685f1cabc665
JulianFP <julian@partanengroup.de>
2026-03-30 11:30:22 +0200

    CI: More schemathesis fixes, but still exclude from CI run

--- tests/project_W_tests/test_schemathesis.py
+++ tests/project_W_tests/test_schemathesis.py
@@ -1,8 +1,9 @@
 import pytest
 import schemathesis
-from schemathesis.core.failures import Failure
+from schemathesis.core.failures import AcceptedNegativeData
 from schemathesis.generation.case import Case
-from schemathesis.openapi.checks import RejectedPositiveData, UndefinedStatusCode
+from schemathesis.openapi.checks import UndefinedStatusCode
+from hypothesis import assume
 
 common_schemathesis_config = {
     "checks": {
@@ -14,13 +15,20 @@ common_schemathesis_config = {
             #seems to be very buggy and unreliable in schemathesis. Constantly authenticated using a correct token while claiming it should have failed.
             "enabled": False,
         },
+        "positive_data_acceptance": {
+            #there are many reasons why data can be rejected even though it is schema compliant. Not everything can be represented in the schema!
+            "enabled": False,
+        },
     },
     "output": {
         "sanitization": {
             # To show the token in the cURL snippet
             "enabled": False
         }
-    }
+    },
+    "headers": {
+        "Connection": "close",
+    },
 }
 
 @pytest.fixture
@@ -35,10 +43,9 @@ def not_authenticated_schema_fixture(backend, get_client):
 @pytest.fixture
 def client_schema_fixture(backend, get_client, get_logged_in_client):
     client = get_logged_in_client(True)
-    config = schemathesis.Config.from_dict({
-        "headers": dict(client.headers),
-        **common_schemathesis_config,
-    })
+    config_dict = common_schemathesis_config
+    config_dict["headers"].update(dict(client.headers))
+    config = schemathesis.Config.from_dict(config_dict)
     return schemathesis.openapi.from_url(
         f"{backend[0]}/openapi.json",
         verify=get_client[1],
@@ -48,10 +55,9 @@ def client_schema_fixture(backend, get_client, get_logged_in_client):
 @pytest.fixture
 def admin_schema_fixture(backend, get_client, get_logged_in_admin_client):
     client = get_logged_in_admin_client(True)
-    config = schemathesis.Config.from_dict({
-        "headers": dict(client.headers),
-        **common_schemathesis_config,
-    })
+    config_dict = common_schemathesis_config
+    config_dict["headers"].update(dict(client.headers))
+    config = schemathesis.Config.from_dict(config_dict)
     return schemathesis.openapi.from_url(
         f"{backend[0]}/openapi.json",
         verify=get_client[1],
@@ -70,17 +76,25 @@ def case_handler(case: Case, get_client):
     try:
         case.validate_response(response)
     #handle exception group:
-    except* Failure as e:
+    except* Exception as eg:
         if response.status_code == 400 and response.text == '':
             #for really weird randomly generated data that some component (FastAPI, granian, python-multipart) cannot parse, the backend will just return code 400 with an empty response
             #schemathesis doesn't like that, because 400 is not documented, or an empty-response (non-json) is not documented
-            pytest.skip("Expected: This is some really weird generated data")
-        if type(e) == RejectedPositiveData and response.status_code == 422 and any(substring in response.text for substring in ["email", "password"]):
-            # expected: email and password validation is too complicated to be represented in the OpenAPI schema. This will
-            pytest.skip("Expected: Password/Email validation stricter than schema")
-        if type(e) == UndefinedStatusCode and response.status_code == 400 and response.text == '{"detail":"There was an error parsing the body"}':
-            # expected: FastAPI returns this if the body is not processable which can happen with randomly generated data
-            pytest.skip("Expected: Unprocessable data")
+            assume(False) #skip current hypothesis example, continue to generate further data though
+        remaining_exceptions = []
+        for e in eg.exceptions:
+            if isinstance(e, UndefinedStatusCode) and response.status_code == 400 and response.text == '{"detail":"There was an error parsing the body"}':
+                # expected: FastAPI returns this if the body is not processable which can happen with randomly generated data
+                continue
+            elif isinstance(e, AcceptedNegativeData) and "Invalid component: Missing `token` at cookie" in str(e):
+                # there is a bug in schemathesis that leads it to interpret multiple authentication methods as additive, instead of interchangeable
+                # the OpenAPI schema defines that either an http bearer or a cookie must be present, but schemathesis interprets this as both must be present
+                continue
+            else:
+                remaining_exceptions.append(e)
+        if len(remaining_exceptions) == 0:
+            assume(False) #skip current hypothesis example, continue to generate further data though
+        raise ExceptionGroup(f"{len(remaining_exceptions)} distinct failures (after filtering)", remaining_exceptions)
 
 @not_authenticated_schema.parametrize()
 def test_schemathesis_unauthenticated(case: Case, get_client):
